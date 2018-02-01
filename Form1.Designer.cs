9fd8460fd7812b6767439f7a4b5ff27436c0b8dc
Ryan <33061569+rsko@users.noreply.github.com>
2018-02-01 13:51:18 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GreenvilleAdvertisement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.femaleButton = new System.Windows.Forms.RadioButton();
+            this.maleButton = new System.Windows.Forms.RadioButton();
+            this.directionsButton = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label1.Location = new System.Drawing.Point(68, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GREENVILLE IDOL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(147, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sign Up Today!";
+            // 
+            // femaleButton
+            // 
+            this.femaleButton.AutoSize = true;
+            this.femaleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.femaleButton.ForeColor = System.Drawing.Color.HotPink;
+            this.femaleButton.Location = new System.Drawing.Point(75, 148);
+            this.femaleButton.Name = "femaleButton";
+            this.femaleButton.Size = new System.Drawing.Size(122, 35);
+            this.femaleButton.TabIndex = 7;
+            this.femaleButton.Text = "Female";
+            this.femaleButton.UseVisualStyleBackColor = true;
+            this.femaleButton.CheckedChanged += new System.EventHandler(this.femaleButton_CheckedChanged);
+            // 
+            // maleButton
+            // 
+            this.maleButton.AutoSize = true;
+            this.maleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maleButton.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.maleButton.Location = new System.Drawing.Point(317, 148);
+            this.maleButton.Name = "maleButton";
+            this.maleButton.Size = new System.Drawing.Size(90, 35);
+            this.maleButton.TabIndex = 8;
+            this.maleButton.Text = "Male";
+            this.maleButton.UseVisualStyleBackColor = true;
+            this.maleButton.CheckedChanged += new System.EventHandler(this.maleButton_CheckedChanged);
+            // 
+            // directionsButton
+            // 
+            this.directionsButton.ForeColor = System.Drawing.Color.Navy;
+            this.directionsButton.Location = new System.Drawing.Point(193, 195);
+            this.directionsButton.Name = "directionsButton";
+            this.directionsButton.Size = new System.Drawing.Size(108, 23);
+            this.directionsButton.TabIndex = 4;
+            this.directionsButton.Text = "Directions";
+            this.directionsButton.UseVisualStyleBackColor = true;
+            this.directionsButton.Click += new System.EventHandler(this.directionsButton_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Green;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(148, 77);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(182, 25);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "greenville-idol.net";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(479, 230);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.directionsButton);
+            this.Controls.Add(this.maleButton);
+            this.Controls.Add(this.femaleButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Advertisement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton femaleButton;
+        private System.Windows.Forms.RadioButton maleButton;
+        private System.Windows.Forms.Button directionsButton;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleAdvertisement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maleButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (maleButton.Checked)
+            {
+                Form2 form2 = new Form2();
+                form2.ShowDialog();
+                maleButton.Checked = false;
+            }
+        }
+
+        private void femaleButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (femaleButton.Checked)
+            {
+                Form3 form3 = new Form3();
+                form3.ShowDialog();
+                femaleButton.Checked = false;
+            }
+        }
+
+        private void directionsButton_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.ShowDialog();
+
+        }
+    }
+}
